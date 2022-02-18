9ce488540ffeae6e1dc3ea796c3adfc854f33961
Filip Hobza <filip.hobza.2005@gmail.com>
2022-02-18 13:29:34 +0100

    18.2.2021

---
+++ cviceniFunkce/cviceniFunkce/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom;
+
+namespace cviceniFunkce
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int cislo1 = 15;
+            float cislo2 = 3.4f;
+
+            float x = test.Test(cislo1, cislo2);
+            Console.WriteLine(x);
+
+        }
+        
+        
+    }
+}
---
+++ cviceniFunkce/cviceniFunkce/test.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace cviceniFunkce
+{
+    public class test
+    {
+        //public float vystup;
+        //public int random;
+        public static float Test(int ahoj, float test)
+        {
+            return ahoj * test;
+            
+        }
+
+        public static float random()
+        {
+            
+        }
+    }
+}
