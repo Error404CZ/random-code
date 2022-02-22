ca125bc0698b93c5a76e796f789b732d9f9f30e4
Filip Hobza <filip.hobza.2005@gmail.com>
2022-02-22 09:38:38 +0100

    Update test.cs

--- cviceniFunkce/cviceniFunkce/test.cs
+++ cviceniFunkce/cviceniFunkce/test.cs
@@ -1,20 +1,24 @@
 using System;
+using System.Security.Cryptography;
 
 namespace cviceniFunkce
 {
     public class test
     {
         //public float vystup;
-        //public int random;
+        public int random;
         public static float Test(int ahoj, float test)
         {
             return ahoj * test;
             
         }
 
-        public static float random()
+        /*
+        public static int randomcislo(int prvnicislo, int druhecislo)
         {
-            
+
+            return Random(prvnicislo, druhecislo);
         }
+        */
     }
 }
