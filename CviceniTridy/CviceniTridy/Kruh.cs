2f0d3f13d411286d2b5f4a6ef735672074e69324
Filip Hobza <filip.hobza.2005@gmail.com>
2022-04-03 18:56:31 +0200

    Random

---
+++ CviceniTridy/CviceniTridy/Kruh.cs
@@ -0,0 +1,29 @@
+﻿
+namespace CviceniTridy
+{
+    class Kruh
+    {
+        public Kruh(int polomer)
+        {
+            this.polomer = polomer;
+        }
+        
+        public int polomer;
+        public int prumer;
+        public float obvod;
+        
+        public int Prumer()
+        {
+            prumer = polomer * 2;
+            return prumer;
+        }
+
+        public float Obvod()
+        {
+            obvod = 2 * 3.14f * polomer;
+            return obvod;
+        }
+        
+        
+    }
+}
---
+++ CviceniTridy/CviceniTridy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace CviceniTridy
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
